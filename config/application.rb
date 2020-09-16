4893b24b13b52aeeb71a355666dad6b98f0e7cfc
Justin Langlinais <justinclanglinais@gmail.com>
2020-09-16 16:29:02 -0700

    feat(application.rb): adjust placement of rack-cors code to be in class

--- config/application.rb
+++ config/application.rb
@@ -28,17 +28,18 @@ module HearthBackend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
 
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-     origins '*'
-     resource '*', :headers => :any, :methods => [:get, :post, :options]
-   end
+    # CORS permissions with rack-cors gem
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
+  end
 end
