a98003bb9c124cc882ff9025b892f450036df659
Justin Langlinais <justinclanglinais@gmail.com>
2020-09-16 15:25:04 -0700

    feat(seeds): create seeds for Cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+    Card.create(
+        name: Faker::Games::WarhammerFantasy.creature,
+        cost: rand(0..10)
+        image: "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/035aab8886b98a7572c1da379210c53b12fae1b02254c86c539adc2fe2cfd661.png"
+        attack: rand(0..12)
+        defense: rand(1..12)
+    )
+end
